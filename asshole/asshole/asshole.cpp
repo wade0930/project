1692e23647292ac2790c2e178c118f163df4cad4
ntutRockman <pen7741@gmail.com>
2017-03-03 09:44:49 +0800

    20170303-09.44

--- asshole/asshole/asshole.cpp
+++ asshole/asshole/asshole.cpp
@@ -3,8 +3,18 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
 	return false;
-}
+}
 int main(array<System::String ^> ^args)
 {
 	int guess;
